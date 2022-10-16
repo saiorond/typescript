aaebf2bb820e1c54abe71497a8f14b30320094a8
Débora <d.saioron@hotmail.com>
2022-10-16 20:53:32 -0300

    objetos

--- index.ts
+++ index.ts
@@ -10,3 +10,26 @@ type User = {
     password?: string;
     orders: Order[];
 }
+
+const user: User = {
+    firstName: "Jane",
+    age: 20,
+    email: "jane@doe.com",
+    orders: [{ productId: "1", price: 200 }],
+}
+
+const printLog = (message: string) => {}
+
+printLog(user.password!);
+
+type Author = {
+    books: string[];
+}
+
+const author: Author & User = {
+    age: 2,
+    books: ["1"],
+    email: "author@gmail.com",
+    firstName: "Felipe",
+    orders: [],
+}
