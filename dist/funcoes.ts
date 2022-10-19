c95a1e753e4a93cfb61ddda29e89b6b32d5c5399
Débora <d.saioron@hotmail.com>
2022-10-18 22:08:31 -0300

    relacionando interfaces com funções

--- dist/funcoes.ts
+++ dist/funcoes.ts
@@ -1,5 +1,13 @@
-const sum = (x: number, y: number): string | number => {
-    return (x + y).toString();
+interface MathFunc {
+    (x: number, y: number): number;
+}
+
+const sum: MathFunc = (x: number, y: number): number => {
+    return x + y;
+}
+
+const sub: MathFunc = (x: number, y:number) => {
+    return x - y;
 }
 
 const value = sum(2, 3);
