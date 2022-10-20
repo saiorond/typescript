2989d332466eb731231a5cf0b1d773da587da983
Débora <d.saioron@hotmail.com>
2022-10-19 21:13:23 -0300

    generics com promises

--- dist/generic.ts
+++ dist/generic.ts
@@ -3,3 +3,15 @@ const returnValue = <T>(value: T): T => value;
 const message = returnValue<string>("Hello World");
 
 const conta = returnValue<number>(5);
+
+function getFirstValueFromArray<Type>(array: Type[]) {
+    return array[0]
+}
+
+const getFirstValueFromStringArray = getFirstValueFromArray<string>(["1", "2"]);
+const getFirstValueFromNumberArray = getFirstValueFromArray<number>([10, 20]);
+
+// promisses
+const returnPromise = async (): Promise<string> => {
+    return "5"
+}
