9f222fe53f67c959fc58c2e0dea078f462c3bd81
Débora <d.saioron@hotmail.com>
2022-10-20 21:48:26 -0300

    generics com classes

--- dist/generic.ts
+++ dist/generic.ts
@@ -11,7 +11,22 @@ function getFirstValueFromArray<Type>(array: Type[]) {
 const getFirstValueFromStringArray = getFirstValueFromArray<string>(["1", "2"]);
 const getFirstValueFromNumberArray = getFirstValueFromArray<number>([10, 20]);
 
-// promisses
+// promises
 const returnPromise = async (): Promise<string> => {
     return "5"
 }
+
+// classes 
+class GenericNumber<T> {
+    zeroValue: T;
+    sum: (x: T, y: T) => T;
+
+    constructor(zeroValue: T, sum: (x: T, y: T) => T) {
+        this.zeroValue = zeroValue;
+        this.sum = sum;
+    }
+}
+
+const myGenericNumber = new GenericNumber<number>(0, (x: number, y: number) => {
+    return x + y;
+})
